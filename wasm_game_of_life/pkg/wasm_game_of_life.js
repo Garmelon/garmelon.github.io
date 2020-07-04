ab5ad60ef955612748894d79d090d2894a480249
Joscha <joscha@plugh.de>
2020-07-04 14:27:37 +0000

    Update wgol

--- wasm_game_of_life/pkg/wasm_game_of_life.js
+++ wasm_game_of_life/pkg/wasm_game_of_life.js
@@ -343,8 +343,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper64 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 30, __wbg_adapter_16);
+    imports.wbg.__wbindgen_closure_wrapper67 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 33, __wbg_adapter_16);
         return addHeapObject(ret);
     };
 
